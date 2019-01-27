4163d60d82a24bbfcc1332d8a86e83ba169d665a
Audi Nugraha <audinue@gmail.com>
2019-01-27 19:12:52 +0700

    Add missing this

--- src/CanvasRenderingContext2D.js
+++ src/CanvasRenderingContext2D.js
@@ -3,9 +3,9 @@ const transform = CanvasRenderingContext2D.prototype.transform
 
 CanvasRenderingContext2D.prototype.transform = function (a, b, c, d, e, f) {
   if (a instanceof DOMMatrixReadOnly) {
-    transform.call(a.a, a.b, a.c, a.d, a.e, a.f)
+    transform.call(this, a.a, a.b, a.c, a.d, a.e, a.f)
   } else {
-    transform.call(a, b, c, d, e, f)
+    transform.call(this, a, b, c, d, e, f)
   }
 }
 
@@ -13,9 +13,9 @@ const fillRect = CanvasRenderingContext2D.prototype.fillRect
 
 CanvasRenderingContext2D.prototype.fillRect = function (x, y, width, height) {
   if (x instanceof DOMRectReadOnly) {
-    fillRect.call(x.x, x.y, x.width, x.height)
+    fillRect.call(this, x.x, x.y, x.width, x.height)
   } else {
-    fillRect.call(x, y, width, height)
+    fillRect.call(this, x, y, width, height)
   }
 }
 
@@ -23,9 +23,9 @@ const strokeRect = CanvasRenderingContext2D.prototype.strokeRect
 
 CanvasRenderingContext2D.prototype.strokeRect = function (x, y, width, height) {
   if (x instanceof DOMRectReadOnly) {
-    strokeRect.call(x.x, x.y, x.width, x.height)
+    strokeRect.call(this, x.x, x.y, x.width, x.height)
   } else {
-    strokeRect.call(x, y, width, height)
+    strokeRect.call(this, x, y, width, height)
   }
 }
 
